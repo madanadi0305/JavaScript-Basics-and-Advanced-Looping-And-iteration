9a27b7438a2abce54463cc4f0a22aa8a50b84088
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 10:25:00 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -3,14 +3,19 @@ function findMax(number){
 var result=0;
 var i=1;
 var k=0;
+var max=0;
 while(i<=number){
     result=result+i;
     if(result===number){
      k=i;
      break;
     }
+    else{
     i=i+1;
+    continue;
+    }
 }
-return k;
+max=k;
+return max;
 }
 module.exports = findMax;
