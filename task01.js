33d11ec8c7de46ea8af0e6af0421376c960ca157
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 09:34:54 +0000

    Update task01.js

--- task01.js
+++ task01.js
@@ -3,7 +3,7 @@ function getMultiple (number) {
 var iterator_value=1;
 var sum1=0;
 while(iterator_value<=10){
-    sum1=sum+number*iterator_value;
+    sum1=sum1+number*iterator_value;
     iterator_value=iterator_value+1;
 }
 return sum1;
