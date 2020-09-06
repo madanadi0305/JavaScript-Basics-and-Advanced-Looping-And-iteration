2352d2eba045a2356d3f0cc0be0cd80c8de4cb75
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 15:58:47 +0000

    Update task05.js

--- task05.js
+++ task05.js
@@ -1,4 +1,11 @@
 function multipleSum(num){
     //  Enter your logic here
+    var sum=0;
+    for(i=1;i<=num;i++){
+        if(i%3==0 || i%5==0){
+            sum=sum+i;
+        }
+    }
+    return sum;
 }
 module.exports = multipleSum;
