c7f762d03abe22c2db614322449b448bf6921bc6
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 16:40:31 +0000

    Update task04.js, wiki/task04.md files

--- task04.js
+++ task04.js
@@ -1,33 +1,21 @@
 function findMax(number){
-// Enter your logic here
-var result=0;
 var i=1;
+var result=0;
 var k=0;
-var max=0;
-
-var previous=0;
 var previous_sum=0;
-
 while(i<=number){
-    
-    result=result+i;
-
-   previous=i-1;
+   result=result+i;
+   var previous=i-1;
    previous_sum=previous_sum+previous;
-
-    if((result===number)){
-     k=i;
-    
-    }
-    else if((number<result)&&(number>previous_sum))
-    {
-        k=previous;
-    }
-    
+   if(result===number){
+       k=i;
+   }
+   else if((number<result)&&(number>=previous_sum)){
+       k=previous;
+   }
     i=i+1;
+}
 
-    
+return k;
 }
-max=k;
-}
-module.exports = findMax;
+module.exports=findMax;
