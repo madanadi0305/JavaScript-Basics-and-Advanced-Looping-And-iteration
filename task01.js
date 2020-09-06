1ee74dea2be3dcedf13fced7a1b6ba78ccb4587b
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 07:18:13 +0000

    Update task01.js

--- task01.js
+++ task01.js
@@ -1,5 +1,12 @@
 function getMultiple (number) {
 // Enter your logic here
+var iterator_value=1;
+var sum=0;
+while(iterator_value<=10){
+    sum=sum+number*iterator_value;
+    iterator_value=iterator_value+1;
+}
+return sum;
 }
 
 module.exports = getMultiple
