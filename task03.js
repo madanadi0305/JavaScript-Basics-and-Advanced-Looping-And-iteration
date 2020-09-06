267075f832792af87af9af054d91749f6a8ac36d
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 09:48:40 +0000

    Update task03.js

--- task03.js
+++ task03.js
@@ -1,4 +1,10 @@
 function solveCube(number){
     // Enter your logic here
+    var i=1;
+    var result=0;
+  while(i<=number){
+      result=i*i*i;
+  }
+    return result;
 }
 module.exports = solveCube;
