19ac310bab2403610a0d8d8ebdadc705d48c3c96
madanadi0305 <madanadi0305@gmail.com>
2020-09-06 12:54:11 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -4,17 +4,17 @@ var result=0;
 var i=1;
 var k=0;
 var max=0;
-//var forward=0;
+
 var previous=0;
 var previous_sum=0;
-//var forward_sum=0;
+
 while(i<=number){
     
     result=result+i;
-   // forward=i+1;
+
    previous=i-1;
    previous_sum=previous_sum+previous;
-    //forward_sum=forward_sum+forward;
+
     if((result===number)){
      k=i;
     
@@ -25,7 +25,7 @@ while(i<=number){
     }
     
     i=i+1;
-    //continue;
+
     
 }
 max=k;
