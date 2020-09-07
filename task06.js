ec850edc4f5c88d623131e694f08f5850e1c7ef5
madanadi0305 <madanadi0305@gmail.com>
2020-09-07 08:50:02 +0000

    Update wiki/task06.md, task06.js files

--- task06.js
+++ task06.js
@@ -1,4 +1,16 @@
 function round(number){
     //  Enter your logic here
+    var i=0;
+    var result=0;
+    var output=0;
+    while(i<=10){
+     result=number+i;   
+     if(result%10===0){
+      output=result;
+     }
+
+     i=i+1;
+    }
+    return output;
 }
 module.exports = round;
