1c877c1b49b9365b1c7cbef62b28dbe2a3a0c55a
madanadi0305 <madanadi0305@gmail.com>
2020-09-07 16:20:14 +0000

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,4 +1,18 @@
-function prime(number){
-    // Enter your logic here
-}
+function prime(n){
+    var i,j=0;
+    for (i=2;i<n;i++){
+        if(n%i===0){
+            j=1;
+        }
+    }
+    if(j===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+    }
+
+
+
 module.exports = prime;
