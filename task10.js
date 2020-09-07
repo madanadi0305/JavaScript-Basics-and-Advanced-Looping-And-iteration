b23cde5f39559f8a5512dd78c25babe29f49c2ad
madanadi0305 <madanadi0305@gmail.com>
2020-09-07 12:15:34 +0000

    Update task10.js

--- task10.js
+++ task10.js
@@ -1,7 +1,7 @@
 function sumSeries(number){
     //  Enter yoru  logic here
     var sum=0;
-    for(var i=0;i<=number;i++){
+    for(var i=1;i<=number;i++){
         sum=sum+(1/i);
     }
     return sum;
