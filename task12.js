e49f966a4448a6e3b44a4d5e6e842e60cd47c950
madanadi0305 <madanadi0305@gmail.com>
2020-09-07 18:15:53 +0000

    Update task12.js

--- task12.js
+++ task12.js
@@ -1,5 +1,20 @@
 function armstong (number) {
     //  Enter yoru logic here
+    var is_Armstrong=false;
+    var n=number;
+    var sum=0;
+    while(n!==0){
+     r=n%10;
+     sum=sum+r*r*r;
+     n=(n/10);
+     if(sum===number){
+         is_Armstrong=true;
+     }
+     else{
+         is_Armstrong=false;
+     }
+    }
+    return is_Armstrong;
 }
 
 module.exports = armstong;
