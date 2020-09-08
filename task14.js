37fb740b53146d002474aac536976a6fbf553593
madanadi0305 <madanadi0305@gmail.com>
2020-09-08 14:38:31 +0000

    Update wiki/task14.md, task14.js files

--- task14.js
+++ task14.js
@@ -1,5 +1,32 @@
-function times(number){
-    //  Enter your logic here
-}
+function times(number) {
+    var n=number;
+    var count=0;
+    var remainder;
+    function digitSum(n){
+    var sum=0;
+    
+    while(n!=0){
+        remainder=n%10;
+        sum=sum+remainder;
+        n=parseInt(n/10);
+        
+        
+    }
+     return sum;
+    
+    }
+    sum1=digitSum(n);
+    count=count+1;
+   
+    
+ 
+        while(sum1>=10){
+            count=count+1;
+            sum1=digitSum(sum1);
+        }
+        
+    
+    return count;
+   }
 
-module.exports = times;
+   module.exports=times
