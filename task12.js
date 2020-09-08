a553b76cf4047c34514daa62f0949826518d020d
madanadi0305 <madanadi0305@gmail.com>
2020-09-08 05:12:27 +0000

    Update task12.js

--- task12.js
+++ task12.js
@@ -3,17 +3,27 @@ function armstong (number) {
     var is_Armstrong=false;
     var n=number;
     var sum=0;
+    var remainder=0;
+    var count=0;
+    var n1=number;
+    while(n1!==0){
+    remainder=n1%10;
+    count=count+1;
+    n=parseInt(n/10);
+
+    }
     while(n!==0){
      r=n%10;
-     sum=sum+r*r*r;
-     n=(n/10);
-     if(sum===number){
+     sum=sum+r^count;
+     n=parseInt(n/10);
+     
+    }
+    if(sum===number){
          is_Armstrong=true;
      }
      else{
          is_Armstrong=false;
      }
-    }
     return is_Armstrong;
 }
 
