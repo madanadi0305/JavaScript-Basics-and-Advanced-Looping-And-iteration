ab930f2548b46a51e3f244f1acff0954e6d3c102
madanadi0305 <madanadi0305@gmail.com>
2020-09-08 09:35:47 +0000

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,17 +1,20 @@
 function prime(n){
-    var i,j=0;
-    for (i=2;i<n;i++){
-        if(n%i===0){
-            j=1;
+    var is_NotPrime=false;
+    var count=0;
+    for(var i=2;i<=number;i++){
+        for(var j=2;j<i;j++){
+            if(i%j===0){
+                is_NotPrime=true;
+                break;
+            }
+
         }
+    if(is_NotPrime===false){
+            
+    count=count+1;
+    }   
     }
-    if(j===0){
-        return true;
-    }
-    else{
-        return false;
-    }
-    }
+}
 
 
 
