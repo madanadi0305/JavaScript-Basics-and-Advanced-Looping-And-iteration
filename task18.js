d44f10acdf33c978f86cffa07eeb73aa76e0e774
madanadi0305 <madanadi0305@gmail.com>
2020-09-09 14:04:39 +0000

    Update task18.js

--- task18.js
+++ task18.js
@@ -1,5 +1,21 @@
 function gcd(num1, num2){
     //  ENter your logic here
+    var n1=number1;
+    var n2=number2;
+    if(n1==0||n2==0)
+    {return 0;
+
+    }
+    if(n1==n2)
+    {return n1;
+
+    }
+    if(n1>n2)
+    {return gcd(n1-n2,n2);}
+    else{
+      return gcd(n1,n2-n1);
+    }
+    
 }
 
 module.exports = gcd;
